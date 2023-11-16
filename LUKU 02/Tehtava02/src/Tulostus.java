da68c95df4ba78367356160fe68b23340451f698
Andy Lang <djdreviz@gmail.com>
2023-11-16 13:01:50 +0200

    Tehtävä 02.01 valmis!

---
+++ LUKU 02/Tehtava02/src/Tulostus.java
@@ -0,0 +1,23 @@
+/*
+Tee toimiva Java-kielinen ohjelma, joka käynnistettäessä tulostaa monitorille lauseen "Terve vaan kaikille!"
+sekä rivinvaihdon huutomerkin jälkeen.
+
+Vihje:
+Kaikki ohjelman tekemiseen tarvittava löytyy tietokonemaailmaan johdattelevan luvun esimerkkiohjelmasta.
+Kun olet ymmärtänyt esimerkin toimintaperiaatteen, sinun pitäisi pystyä tekemään ohjelma helposti.
+Merkkijonoja tulostettaessa rivinvaihdon merkkijonon loppuun saa helposti käyttämällä println()-metodia.
+Metodin käytön voit katsoa manuaalista.
+
+Ohjelma kirjoitetaan luokkaan, jonka nimi on Tulostus.
+
+Esimerkkituloste:
+Terve vaan kaikille!
+ */
+
+public class Tulostus
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Terve vaan kaikille!");
+    }
+}
